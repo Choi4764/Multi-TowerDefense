946a55d276a5573cf130fb75082876022bbbc3e6
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-04 20:08:01 +0900

    DB관련 코드 추가

---
+++ src/db/database.js
@@ -0,0 +1,37 @@
+import mysql from 'mysql2/promise.js';
+import { config } from '../config/config.js';
+
+const { database } = config;
+
+const createPool = (dbConfig) => {
+  const pool = mysql.createPool({
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.name,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  const originQuery = pool.query;
+  pool.query = (sql, params) => {
+    const date = new Date();
+    console.log(
+      // TODO: formatDate 추가하기
+      `[${date}] Executing query: ${sql} ${
+        params ? `, ${JSON.stringify(params)}` : ``
+      }`,
+    );
+    return originQuery.call(pool, sql, params);
+  };
+
+  return pool;
+};
+
+const pools = {
+  USER_DB: createPool(database.USER_DB),
+};
+
+export default pools;

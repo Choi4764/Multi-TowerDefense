6fde5dc378e1aa19f8d76690734dedbce72d7d66
doughnutdough5 <145208718+doughnutdough5@users.noreply.github.com>
2024-11-05 17:30:36 +0900

    onEnd, onError 이벤트 등록

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -1,8 +1,11 @@
 import { onData } from './onData.js';
+import { onEnd } from './onEnd.js';
 
 export const onConnection = (socket) => {
   console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
   socket.buffer = Buffer.alloc(0);
 
   socket.on('data', onData(socket));
+  socket.on('end', onEnd(socket));
+  socket.on('error', onError(socket));
 };

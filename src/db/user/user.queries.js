8e068f433e4aa3de33bc9230ea51c0dfaa40812c
hanwj75 <wjws9608@gmail.com>
2024-11-08 19:39:52 +0900

    delete : "user.queries.js삭제"
    sql.queries.js로 통합

--- src/db/user/user.queries.js
+++
@@ -1,5 +0,0 @@
-export const SQL_QUERIES = {
-  FIND_USER_BY_ID: 'SELECT * FROM user WHERE userId = ?',
-  CREATE_USER: 'INSERT INTO user (email, userId, password) VALUES (?, ?, ?)',
-  UPDATE_USER_LOGIN_TIMESTAMP: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE userId = ?',
-};

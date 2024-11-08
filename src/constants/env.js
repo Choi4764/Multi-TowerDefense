da3e307f6c55b68f9177a81f549ac80490ee006c
hanwj75 <wjws9608@gmail.com>
2024-11-08 19:41:01 +0900

    Update : "환경변수 추가"

--- src/constants/env.js
+++ src/constants/env.js
@@ -12,7 +12,7 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PORT = process.env.DB_PORT || 3306;
 
-export const DB2_NAME = process.env.DB2_NAME || 'recode_db';
+export const DB2_NAME = process.env.DB2_NAME || 'record_db';
 export const DB2_USER = process.env.DB2_USER || 'root';
 export const DB2_PASSWORD = process.env.DB2_PASSWORD || 'password';
 export const DB2_HOST = process.env.DB2_HOST || 'localhost';

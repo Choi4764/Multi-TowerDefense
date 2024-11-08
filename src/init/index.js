21b19acb865877008f965e0981648498d69bbc4b
hanwj75 <wjws9608@gmail.com>
2024-11-08 19:47:26 +0900

    Fix : "에러처리"

--- src/init/index.js
+++ src/init/index.js
@@ -1,19 +1,16 @@
-
 import { v4 as uuidv4 } from 'uuid';
 import { loadProto } from './loadProtos.js';
 import { testAllConnections } from '../utils/db/testConnection.js';
 import pools from '../db/database.js';
 
-
 const initServer = async () => {
-    try {
-        await loadProto();
-        // await testAllConnections(pools);
-
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+  try {
+    await loadProto();
+    // await testAllConnections(pools);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 export default initServer;
